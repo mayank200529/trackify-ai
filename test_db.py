9e69b3c933d239c1441b6b9a99d92625e696d05c
Mayank Singh Rawat <rawatmayank685@gmail.com>
2026-05-31 04:23:33 +0530

    Setup Flask project and MySQL connection

---
+++ test_db.py
@@ -0,0 +1,8 @@
+from db import get_db_connection
+
+try:
+    conn = get_db_connection()
+    print("Database connected successfully!")
+    conn.close()
+except Exception as e:
+    print("Error:", e)
