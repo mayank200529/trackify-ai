1047b8bee5b82e6e002050de077318e2ea9ca577
Mayank Singh Rawat <rawatmayank685@gmail.com>
2026-05-31 04:30:31 +0530

    Completed - Flask templates and routing

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Trackify AI is running successfully!"
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
